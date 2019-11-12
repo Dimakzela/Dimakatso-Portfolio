7262b46a7a83ab066beb040c395968b64798efaf
Bopape, Dimakatso D <dimakatso.bopape@standardbank.co.za>
2019-11-12 09:21:00 +0200

    removed target

--- dimakatso-portfolio-service/target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO contact_details (id, email_address, cell_no)
-VALUES (1, 'dimakzela@gmail.com', '0730914101');
-
-INSERT INTO address (id, town, suburb, area_code)
-VALUES (1, 'Midrand', 'Ebony Park Ext 6', 1632);
-
-INSERT INTO personal_info (id, first_name, last_name, date_of_birth, contact_details, address)
-VALUES (1, 'Dimakatso', 'Bopape', PARSEDATETIME('26 Jul 2016','dd MMM yyyy','en'), 1, 1);
